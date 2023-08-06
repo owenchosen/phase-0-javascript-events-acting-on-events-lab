404ebc3a408490fca7a3baae7728fa0eb9848bb4
owen <owen.chosen@gmail.com>
2023-08-06 22:05:34 +0100

    Done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+const dodger = document.getElementById ("dodger");
+const game = document.getElementById ("game");
+dodger.style.bottom = "100px";
+dodger.style.backgroundColor = "#FF69B4";
+
+
+
+document.addEventListener ("keydown", (eventDetails) => {
+    if (eventDetails.key === "ArrowLeft") moveDodgerLeft();
+    else if (eventDetails.key === "ArrowRight") moveDodgerRight();
+    else{}
+});
+
+function moveDodgerLeft () {
+    let left = dodger.style.left.replace('px', '');
+    left = parseInt(left, 10);
+    if (left > 0) dodger.style.left = `${left - 1}px`;
+}
+
+function moveDodgerRight () {
+    let left = dodger.style.left.replace('px', '');
+    left = parseInt(left, 10);
+    if (left < 360) dodger.style.left = `${left + 1}px`;
+}
